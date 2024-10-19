05ac704a90e4c453b2bbd87968933db1fe97720d
JusteAnuze <simrenka@gmail.com>
2024-10-19 14:30:55 +0300

    change media

--- script.js
+++ script.js
@@ -1,22 +1,43 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const form = document.getElementById('contact');
+    const textInput = document.getElementById('textInput');
+    const emailInput = document.getElementById('emailInput');
+    const phoneInput = document.getElementById('phoneInput');
+    const cityInput = document.getElementById ('cityInput');
+    form.addEventListener('submit', (event) => {
+        let valid = true;
+        let message = '';
+        if (textInput.value.trim() === '') {
+            message += 'Text field cannot be empty!\n';
+            valid = false;
+        }
+        if (emailInput.value.trim() === '') {
+            message += 'Email field cannot be empty!\n';
+            valid = false;
+        } 
+        else if (!/\S+@\S+\.\S+/.test(emailInput.value.trim())) {
+            message += 'Invalid email format!\n';
+            valid = false;
+        }
+        if (phoneInput.value.trim() === '') {
+            message += 'PhoneNumber field cannot be empty!\n';
+            valid = false;
+        } 
+        else if (!/^\d{9}$/.test(phoneInput.value.trim())) {
+            message += 'Phone number must contain exactly 9 digits!\n';
+            valid = false;
+        }
+        if (!valid) {
+            alert(message);
+            event.preventDefault();  
+        }
+    });
+});
+document.addEventListener("DOMContentLoaded", () => {
+    const burger = document.querySelector(".burger");
+    const navLinks = document.querySelector(".nav-links");
 
-const button = document.querySelector('.button');
-const backgroundColor = window.getComputedStyle(button).backgroundColor;
-
-// Now you can use this color in other parts of your JavaScript
-console.log('The button background color is:', backgroundColor);
-
-function openModal() {
-    document.getElementById("myModal").style.display = "block";
-}
-
-// Close the modal
-function closeModal() {
-    document.getElementById("myModal").style.display = "none";
-}
-
-// Close the modal when clicking outside of it
-window.onclick = function(event) {
-    if (event.target == document.getElementById("myModal")) {
-        closeModal();
-    }
-}
+    // Paspaudus burger mygtuką, keičiamas navigacijos meniu matomumas
+    burger.addEventListener("click", () => {
+        navLinks.classList.toggle("active");
+    });
